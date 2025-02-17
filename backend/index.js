28a2d9f22e1764287ce7b240d4b6bb4da4a1925a
Belen Canalda <belencanalda5@gmail.com>
2025-02-17 18:27:14 -0300

    mejoras en rutas

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 import app from "./app.js";
 import { connetDb } from "./db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 connetDb();
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
     console.log("Servidor corriendo en el puerto 3000")
 });
--- backend/routes/reservas.routes.js
+++ backend/routes/reservas.routes.js
@@ -3,6 +3,7 @@ import { getReservas, createReserva } from "../controllers/reserva.controller.js
 
 const router = Router();
 
+router.get("/", (req, res) => res.send("Bienvenido a la cafeteria"));
 router.get("/reservas", getReservas);
 router.post("/reservas", createReserva);
 
